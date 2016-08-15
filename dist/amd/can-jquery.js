cb87d52a7ac26ff9ae36bfd0d40c62b3fd4c349d
Matthew Phillips <matthew@matthewphillips.info>
2016-08-15 12:50:44 -0400

    3.0.0-pre.1

---
+++ dist/amd/can-jquery.js
@@ -0,0 +1,6 @@
+/*can-jquery@3.0.0-pre.0#can-jquery*/
+define(function (require, exports, module) {
+    var $ = require('jquery');
+    var ns = require('can-util/namespace');
+    module.exports = ns.$ = $;
+});
---
+++ dist/cjs/can-jquery.js
@@ -0,0 +1,4 @@
+/*can-jquery@3.0.0-pre.0#can-jquery*/
+var $ = require('jquery');
+var ns = require('can-util/namespace');
+module.exports = ns.$ = $;
