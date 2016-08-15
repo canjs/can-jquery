3c038dd9e92917e003363a716609bb87f3d372b8
Matthew Phillips <matthew@matthewphillips.info>
2016-08-15 16:40:26 -0400

    3.0.0-pre.2

---
+++ dist/amd/can-jquery.js
@@ -0,0 +1,25 @@
+/*can-jquery@3.0.0-pre.1#can-jquery*/
+define(function (require, exports, module) {
+    var $ = require('jquery');
+    var ns = require('can-util/namespace');
+    var domEvents = require('can-util/dom/events');
+    module.exports = ns.$ = $;
+    function setupSpecialEvent(eventName) {
+        var handler = function () {
+            $(this).trigger(eventName);
+        };
+        $.event.special[eventName] = {
+            setup: function () {
+                domEvents.addEventListener.call(this, eventName, handler);
+            },
+            teardown: function () {
+                domEvents.removeEventListener.call(this, eventName, handler);
+            }
+        };
+    }
+    [
+        'inserted',
+        'removed',
+        'attributes'
+    ].forEach(setupSpecialEvent);
+});
