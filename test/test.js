cb9b330a2f8cc5f17a31fa4424d69c379d707e05
Kevin Phillips <kphillips86@gmail.com>
2016-10-06 17:17:32 -0500

    Test cleanup and adding tests of other projects
    Other projects could be affected by can-jquery's behavior.
    This runs the tests for those projects alongside the tests for can-jquery.

--- test/test.js
+++ test/test.js
@@ -1,3 +1,9 @@
-import '../can-jquery_test';
-import '../can-jquery-stache-bindings_test';
-import '../legacy_test';
+require('can-jquery');
+
+require('../can-jquery_test');
+
+require('can-component/test/component-test');
+require('can-control/can-control_test');
+require('can-stache/test/stache-test');
+require('can-stache-bindings/test/bindings-test');
+require('can-util/test/test');
