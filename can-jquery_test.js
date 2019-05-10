98789a3ac6c6c6b3ee8b871c5f332b4225b2aa69
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2019-05-10 18:53:22 +0100

    Landscaper: QUnit2 upgrade

--- can-jquery_test.js
+++ can-jquery_test.js
@@ -17,18 +17,18 @@ require("can-util/dom/events/removed/removed");
 require("can-stache-bindings");
 
 QUnit.module("can-jquery/legacy - can-controls", {
-	setup: function() {
+	beforeEach: function(assert) {
 		enableLegacyMode($);
 	},
-	teardown: function() {
+	afterEach: function(assert) {
 		disableLegacyMode();
 	}
 });
 
-QUnit.test("this.element is jQuery wrapped", function(){
+QUnit.test("this.element is jQuery wrapped", function(assert) {
 	var MyThing = Control.extend({
 		init: function(){
-			QUnit.ok(this.element instanceof $, "it is jQuery wrapped");
+			assert.ok(this.element instanceof $, "it is jQuery wrapped");
 		}
 	});
 
@@ -37,111 +37,111 @@ QUnit.test("this.element is jQuery wrapped", function(){
 });
 
 QUnit.module("can-jquery/legacy - inserted/removed", {
-	setup: function() {
+	beforeEach: function(assert) {
 		enableLegacyMode($);
 	},
-	teardown: function() {
+	afterEach: function(assert) {
 		disableLegacyMode();
 	}
 });
 
-QUnit.test("inserted is triggered", function(){
+QUnit.test("inserted is triggered", function(assert) {
 	var $el = $("<div>");
 
 	$el.on("inserted", function(){
-		QUnit.ok(true, "inserted did fire");
+		assert.ok(true, "inserted did fire");
 
-		QUnit.start();
+		done();
 	});
 
 	mutate.appendChild.call($("#qunit-fixture")[0], $el[0]);
 
-	QUnit.stop();
+	var done = assert.async();
 });
 
-QUnit.test("inserted does not bubble", function(){
-	expect(2);
+QUnit.test("inserted does not bubble", function(assert) {
+	assert.expect(2);
 	var $div = $("<div>");
 	var $span = $("<span>");
 
 	$div.on("inserted", function(){
-		QUnit.ok(true, "inserted fired for div");
+		assert.ok(true, "inserted fired for div");
 	});
 
 	$span.on("inserted", function(){
-		QUnit.ok(true, "inserted fired for span");
+		assert.ok(true, "inserted fired for span");
 	});
 
 	mutate.appendChild.call($("#qunit-fixture")[0], $div[0]);
 
 	mutate.appendChild.call($div[0], $span[0]);
 
-	QUnit.stop();
+	var done = assert.async();
 
 	setTimeout(function() {
-		QUnit.start();
+		done();
 	}, 50);
 });
 
-QUnit.test("inserted is triggered without MutationObserver", function(){
+QUnit.test("inserted is triggered without MutationObserver", function(assert) {
 	globals.setKeyValue('MutationObserver', null);
 
 	var $el = $("<div>");
 
 	$el.on("inserted", function(){
-		QUnit.ok(true, "inserted did fire");
+		assert.ok(true, "inserted did fire");
 
-		QUnit.start();
+		done();
 
 		globals.reset('MutationObserver');
 	});
 
 	mutate.appendChild.call($("#qunit-fixture")[0], $el[0]);
 
-	QUnit.stop();
+	var done = assert.async();
 });
 
-QUnit.test("inserted is triggered without MutationObserver going through jQuery", function(){
+QUnit.test("inserted is triggered without MutationObserver going through jQuery", function(assert) {
 	globals.setKeyValue('MutationObserver', null);
 
 	var $el = $("<div>");
 
 	$el.on("inserted", function(){
-		QUnit.ok(true, "inserted did fire");
+		assert.ok(true, "inserted did fire");
 
-		QUnit.start();
+		done();
 
 		globals.reset('MutationObserver');
 	});
 
 	$("#qunit-fixture").append($el);
 
-	QUnit.stop();
+	var done = assert.async();
 
 });
 
-QUnit.test("inserted should not use $.trigger", function(){
+QUnit.test("inserted should not use $.trigger", function(assert) {
 	var $el = $("div>");
 
 	$el.on("inserted", function(ev, arg1, arg2){
-		QUnit.ok(arg1 === undefined, "should not pass arg1");
-		QUnit.ok(arg2 === undefined, "should not pass arg2");
-		QUnit.ok(true, "inserted did fire");
+		assert.ok(arg1 === undefined, "should not pass arg1");
+		assert.ok(arg2 === undefined, "should not pass arg2");
+		assert.ok(true, "inserted did fire");
 
-		QUnit.start();
+		done();
 	});
 
-	QUnit.stop();
+	var done = assert.async();
 	domEvents.dispatch.call($el[0], "inserted", ["foo", "bar"]);
 });
 
-QUnit.test("removed is triggered", function(){
+QUnit.test("removed is triggered", function(assert) {
 	var $el = $("<div>");
 
 	$el.on("removed", function(){
-		QUnit.ok(true, "removed did fire");
+		assert.ok(true, "removed did fire");
 
-		QUnit.start();
+		done();
 	});
 
 	var fixture = $("#qunit-fixture")[0];
@@ -149,18 +149,18 @@ QUnit.test("removed is triggered", function(){
 	mutate.appendChild.call(fixture, $el[0]);
 	mutate.removeChild.call(fixture, $el[0]);
 
-	QUnit.stop();
+	var done = assert.async();
 });
 
-QUnit.test("removed is triggered without MutationObserver", function(){
+QUnit.test("removed is triggered without MutationObserver", function(assert) {
 	globals.setKeyValue('MutationObserver', null);
 
 	var $el = $("<div>");
 
 	$el.on("removed", function(){
-		QUnit.ok(true, "removed did fire");
+		assert.ok(true, "removed did fire");
 
-		QUnit.start();
+		done();
 
 		globals.reset('MutationObserver');
 	});
@@ -170,18 +170,18 @@ QUnit.test("removed is triggered without MutationObserver", function(){
 	mutate.appendChild.call(fixture, $el[0]);
 	mutate.removeChild.call(fixture, $el[0]);
 
-	QUnit.stop();
+	var done = assert.async();
 });
 
-QUnit.test("removed is triggered without MutationObserver through jQuery", function(){
+QUnit.test("removed is triggered without MutationObserver through jQuery", function(assert) {
 	globals.setKeyValue('MutationObserver', null);
 
 	var $el = $("<div>");
 
 	$el.on("removed", function(){
-		QUnit.ok(true, "removed did fire");
+		assert.ok(true, "removed did fire");
 
-		QUnit.start();
+		done();
 		
 		globals.reset('MutationObserver');
 	});
@@ -193,37 +193,37 @@ QUnit.test("removed is triggered without MutationObserver through jQuery", funct
 	var fixture = $("#qunit-fixture");
 	fixture.append($el);
 
-	QUnit.stop();
+	var done = assert.async();
 });
 
-QUnit.test("removed should not use $.trigger", function(){
+QUnit.test("removed should not use $.trigger", function(assert) {
 	var $el = $("div>");
 
 	$el.on("removed", function(ev, arg1, arg2){
-		QUnit.ok(arg1 === undefined, "should not pass arg1");
-		QUnit.ok(arg2 === undefined, "should not pass arg2");
-		QUnit.ok(true, "removed did fire");
+		assert.ok(arg1 === undefined, "should not pass arg1");
+		assert.ok(arg2 === undefined, "should not pass arg2");
+		assert.ok(true, "removed did fire");
 
-		QUnit.start();
+		done();
 	});
 
-	QUnit.stop();
+	var done = assert.async();
 	domEvents.dispatch.call($el[0], "removed", ["foo", "bar"]);
 });
 
 QUnit.module("can-jquery/legacy - custom jQuery events", {
-	setup: function() {
+	beforeEach: function(assert) {
 		enableLegacyMode($);
 	},
-	teardown: function() {
+	afterEach: function(assert) {
 		disableLegacyMode();
 	}
 });
 
-QUnit.test("fire within controls", function(){
+QUnit.test("fire within controls", function(assert) {
 	var MyControl = Control.extend({
 		"some-event": function(){
-			QUnit.ok(true, "some-event fired");
+			assert.ok(true, "some-event fired");
 		}
 	});
 
@@ -233,15 +233,15 @@ QUnit.test("fire within controls", function(){
 	div.trigger("some-event");
 });
 
-QUnit.test("receives data passed to $.trigger", function(){
+QUnit.test("receives data passed to $.trigger", function(assert) {
 	var MyControl = Control.extend({
 		"names-added": function(el, ev, first, second, third){
-			QUnit.equal(el[0].nodeName, "DIV", "element is the first arg");
-			QUnit.ok(ev instanceof $.Event, "second arg is a jQuery Event");
+			assert.equal(el[0].nodeName, "DIV", "element is the first arg");
+			assert.ok(ev instanceof $.Event, "second arg is a jQuery Event");
 
-			QUnit.equal(first, "Matthew");
-			QUnit.equal(second, "David");
-			QUnit.equal(third, "Brian");
+			assert.equal(first, "Matthew");
+			assert.equal(second, "David");
+			assert.equal(third, "Brian");
 		}
 	});
 
@@ -256,15 +256,15 @@ QUnit.test("receives data passed to $.trigger", function(){
 	]);
 });
 
-QUnit.test("receives data passed to $.trigger when using domEvents.dispatch", function() {
+QUnit.test("receives data passed to $.trigger when using domEvents.dispatch", function(assert) {
 	var MyControl = Control.extend({
 		"names-added": function(el, ev, first, second, third){
-			QUnit.equal(el[0].nodeName, "DIV", "element is the first arg");
-			QUnit.ok(ev instanceof $.Event, "second arg is a jQuery Event");
+			assert.equal(el[0].nodeName, "DIV", "element is the first arg");
+			assert.ok(ev instanceof $.Event, "second arg is a jQuery Event");
 
-			QUnit.equal(first, "Matthew");
-			QUnit.equal(second, "David");
-			QUnit.equal(third, "Brian");
+			assert.equal(first, "Matthew");
+			assert.equal(second, "David");
+			assert.equal(third, "Brian");
 		}
 	});
 
@@ -279,15 +279,15 @@ QUnit.test("receives data passed to $.trigger when using domEvents.dispatch", fu
 	]);
 });
 
-QUnit.test("receives data passed when delegating", function(){
+QUnit.test("receives data passed when delegating", function(assert) {
 	var MyControl = Control.extend({
 		"ul	names-added": function(el, ev, first, second, third){
-			QUnit.equal(el[0].nodeName, "UL", "element is the first arg");
-			QUnit.ok(ev instanceof $.Event, "second arg is a jQuery Event");
+			assert.equal(el[0].nodeName, "UL", "element is the first arg");
+			assert.ok(ev instanceof $.Event, "second arg is a jQuery Event");
 
-			QUnit.equal(first, "Matthew");
-			QUnit.equal(second, "David");
-			QUnit.equal(third, "Brian");
+			assert.equal(first, "Matthew");
+			assert.equal(second, "David");
+			assert.equal(third, "Brian");
 		}
 	});
 
@@ -303,52 +303,52 @@ QUnit.test("receives data passed when delegating", function(){
 });
 
 QUnit.module("can-jquery/legacy - Regular dom events", {
-	setup: function() {
+	beforeEach: function(assert) {
 		enableLegacyMode($);
 	},
-	teardown: function() {
+	afterEach: function(assert) {
 		disableLegacyMode();
 	}
 });
 
-QUnit.test("Only fires once", function(){
+QUnit.test("Only fires once", function(assert) {
 	QUnit.expect(1);
 
 	var el = $("<div>");
 
 	domEvents.addEventListener.call(el[0], "click", function(){
-		QUnit.ok(true);
+		assert.ok(true);
 	});
 
 	el.trigger("click");
 });
 
 QUnit.module("can-jquery/legacy - $.fn.viewModel()", {
-	setup: function() {
+	beforeEach: function(assert) {
 		enableLegacyMode($);
 	},
-	teardown: function() {
+	afterEach: function(assert) {
 		disableLegacyMode();
 	}
 });
 
-QUnit.test("Gets an element's viewModel", function(){
+QUnit.test("Gets an element's viewModel", function(assert) {
 	var el = $("<div>");
 	var map = new CanMap();
 
 	domData.set.call(el[0], "viewModel", map);
 
-	QUnit.equal(el.viewModel(), map, "returns the map instance");
+	assert.equal(el.viewModel(), map, "returns the map instance");
 });
 
 
-QUnit.test("multiple times fired (#21)", function(){
+QUnit.test("multiple times fired (#21)", function(assert) {
 	QUnit.expect(2);
 
 	var el = $("<div>");
 
 	domEvents.addEventListener.call(el[0], "click", function(){
-		QUnit.ok(true);
+		assert.ok(true);
 	});
 
 	el.trigger("click");
@@ -356,23 +356,23 @@ QUnit.test("multiple times fired (#21)", function(){
 });
 
 QUnit.module("can-jquery/legacy - trigger extraParameters", {
-	setup: function() {
+	beforeEach: function(assert) {
 		enableLegacyMode($);
 	},
-	teardown: function() {
+	afterEach: function(assert) {
 		disableLegacyMode();
 	}
 });
 
-QUnit.test("can-event passes extra args to handler", function () {
-	expect(3);
+QUnit.test("can-event passes extra args to handler", function(assert) {
+	assert.expect(3);
 	var template = stache("<p can-myevent='handleMyEvent'>{{content}}</p>");
 
 	var frag = template({
 		handleMyEvent: function(context, el, event, arg1, arg2) {
-			QUnit.ok(true, "handleMyEvent called");
-			QUnit.equal(arg1, "myarg1", "3rd argument is the extra event args");
-			QUnit.equal(arg2, "myarg2", "4rd argument is the extra event args");
+			assert.ok(true, "handleMyEvent called");
+			assert.equal(arg1, "myarg1", "3rd argument is the extra event args");
+			assert.equal(arg2, "myarg2", "4rd argument is the extra event args");
 		}
 	});
 
@@ -382,16 +382,16 @@ QUnit.test("can-event passes extra args to handler", function () {
 	canEvent.trigger.call(p0, "myevent", ["myarg1", "myarg2"]);
 });
 
-QUnit.test("extra args to handler can be read using `%arguments`", function () {
-	expect(4);
+QUnit.test("extra args to handler can be read using `%arguments`", function(assert) {
+	assert.expect(4);
 	var template = stache("<p can-myevent='handleMyEvent(%arguments)'>{{content}}</p>");
 
 	var frag = template({
 		handleMyEvent: function(args) {
-			QUnit.ok(true, "handleMyEvent called");
-			QUnit.ok(args[0] instanceof $.Event, "args[0] is a jquery event");
-			QUnit.equal(args[1], "myarg1", "args[1] is the extra event args");
-			QUnit.equal(args[2], "myarg2", "args[2] is the extra event args");
+			assert.ok(true, "handleMyEvent called");
+			assert.ok(args[0] instanceof $.Event, "args[0] is a jquery event");
+			assert.equal(args[1], "myarg1", "args[1] is the extra event args");
+			assert.equal(args[2], "myarg2", "args[2] is the extra event args");
 		}
 	});
 
@@ -403,15 +403,15 @@ QUnit.test("extra args to handler can be read using `%arguments`", function () {
 
 QUnit.module("can-jquery - addEventListener / removeEventListener");
 
-QUnit.test("should not trigger events on Document Fragments", function() {
-	expect(0);
+QUnit.test("should not trigger events on Document Fragments", function(assert) {
+	assert.expect(0);
 	var origOn = $.fn.on;
 	var origOff = $.fn.off;
 	$.fn.on = function() {
-		QUnit.ok(false, 'should not set up jQuery event listener');
+		assert.ok(false, 'should not set up jQuery event listener');
 	};
 	$.fn.off = function() {
-		QUnit.ok(false, 'should not remove jQuery event listener');
+		assert.ok(false, 'should not remove jQuery event listener');
 	};
 
 	var el = document.createDocumentFragment();
@@ -428,12 +428,12 @@ QUnit.test("should call correct `removed` handler when one is removed", function
 	var $el = $("<div>");
 
 	var teardownOne = function() {
-		QUnit.ok(false, "removed event listener should not be called");
+		assert.ok(false, "removed event listener should not be called");
 		done();
 	};
 
 	var teardownTwo = function() {
-		QUnit.ok(true, "event listener should be called");
+		assert.ok(true, "event listener should be called");
 		// Must be async to avoid race condition
 		done();
 	};
@@ -449,7 +449,7 @@ QUnit.test("should call correct `removed` handler when one is removed", function
 });
 
 
-QUnit.test("should call beforeremove before removed", function() {
+QUnit.test("should call beforeremove before removed", function(assert) {
 
 	var beforeRemoveCalls = 0;
 
@@ -474,6 +474,6 @@ QUnit.test("should call beforeremove before removed", function() {
 	$playground.append(tmpl());
 	$playground.empty();
 
-	equal(beforeRemoveCalls, 3, 'calls beforeremove 3 times when component is removed from page');
+	assert.equal(beforeRemoveCalls, 3, 'calls beforeremove 3 times when component is removed from page');
 
 });
